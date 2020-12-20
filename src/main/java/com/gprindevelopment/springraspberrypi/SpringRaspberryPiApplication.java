40e866e35264282f86cbe71c2a6c148e8382a19e
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-12-20 14:47:17 -0300

    Spring initializer

---
+++ src/main/java/com/gprindevelopment/springraspberrypi/SpringRaspberryPiApplication.java
@@ -0,0 +1,13 @@
+package com.gprindevelopment.springraspberrypi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRaspberryPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRaspberryPiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gprindevelopment/springraspberrypi/SpringRaspberryPiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gprindevelopment.springraspberrypi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRaspberryPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
